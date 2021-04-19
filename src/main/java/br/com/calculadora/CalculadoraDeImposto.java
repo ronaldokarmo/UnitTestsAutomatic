aa640a493d9aa33ca646ee4b915ee446f08b65f6
Ronaldo do Carmo <ronaldokarmo@gmail.com>
2021-04-19 19:32:55 -0300

    Start project

---
+++ src/main/java/br/com/calculadora/CalculadoraDeImposto.java
@@ -0,0 +1,36 @@
+package br.com.calculadora;
+
+public class CalculadoraDeImposto {
+
+	private double aliquota;
+
+	public double calculoAliquota(double valor) {
+		
+		if (valor > 4000) {
+			aliquota = 0.22f;
+			valor += (valor * aliquota);
+		}
+		
+		else if (valor >= 2500.01 && valor <= 4000) {
+			aliquota = 0.15f;
+			valor += (valor * aliquota);
+
+		} 
+
+		else if (valor >= 1000.01 && valor <= 2500) {
+			aliquota = 0.10f;
+			valor += (valor * aliquota);
+
+		} 
+		else if (valor > 0 && valor <= 1000) {
+			aliquota = 0.5f;
+			valor += (valor * aliquota);
+		}
+
+		else {
+			valor = 0;
+		}
+
+		return valor;
+	}
+}
